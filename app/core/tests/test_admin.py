9b96ad04f6f6cbc59bb62f9301f4b813e55ad724
Radomir Niewiadomski <radomirn96@gmail.com>
2023-02-08 16:38:55 +0100

    Add swagger /fixed

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -11,6 +11,7 @@ class AdminSiteTests(TestCase):
     """Tests for Django admin."""
 
     def setUp(self):
+        """Create user and client."""
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
             email='admin@example.com',
